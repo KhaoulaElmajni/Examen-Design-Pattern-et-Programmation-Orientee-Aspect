bf5c538db1a2c126b40453841165bd4391523548
Khaoula Elmajni <elmajnikhaoula99@gmail.com>
2022-12-23 12:23:33 +0100

    final version

--- ExamDP-ELMAJNI/src/main/java/me/elmajni/examdpelmajni/entities/DiagrammeDeClasse.java
+++ ExamDP-ELMAJNI/src/main/java/me/elmajni/examdpelmajni/entities/DiagrammeDeClasse.java
@@ -12,10 +12,44 @@ import java.util.List;
 @Component
 public class DiagrammeDeClasse  {
 
+
     private IStratgyGenerateCode generateCode;
     private IStrategyExport Export;
     private  List<Entite> entites = new ArrayList<>();
     private List<Methode> methodes= new ArrayList<>();
+
+    public DiagrammeDeClasse() {
+    }
+
+    @Log
+    public IStratgyGenerateCode getGenerateCode() {
+        return generateCode;
+    }
+
+    @Log
+    public IStrategyExport getExport() {
+        return Export;
+    }
+
+    @Log
+    public List<Entite> getEntites() {
+        return entites;
+    }
+
+    @Log
+    public void setEntites(List<Entite> entites) {
+        this.entites = entites;
+    }
+
+    @Log
+    public List<Methode> getMethodes() {
+        return methodes;
+    }
+
+    @Log
+    public void setMethodes(List<Methode> methodes) {
+        this.methodes = methodes;
+    }
 
     @Log
     public void setGenerateCode(IStratgyGenerateCode generateCode) {
--- ExamDP-ELMAJNI/src/main/java/me/elmajni/examdpelmajni/test/AppTest.java
+++ ExamDP-ELMAJNI/src/main/java/me/elmajni/examdpelmajni/test/AppTest.java
@@ -5,15 +5,28 @@ import me.elmajni.examdpelmajni.design_patterns.ExportDiagrammeBinaire;
 import me.elmajni.examdpelmajni.design_patterns.GenerateCodeJava;
 import me.elmajni.examdpelmajni.entities.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class AppTest {
 
     public static void main(String[] args) {
 
         System.out.println("****************************Début de test du programme****************************");
 
+        List<Entite> entites = new ArrayList<>();
+        entites.add(new Classe("ClasseDefaut"));
+        entites.add(new GroupeClasse("GroupeClasse"));
+        entites.add(new Interface("Interface"));
+        entites.add(new Enumerateur("Enumerateur"));
+        entites.add(new Classe("EntiteDestination"));
+
+
         DiagrammeDeClasse diagrammeDeClasse = new DiagrammeDeClasse();
+        diagrammeDeClasse.setEntites(entites);
         diagrammeDeClasse.setGenerateCode(new GenerateCodeJava());
         diagrammeDeClasse.genererCode();
+
 
         GroupeClasse groupeClasse = new GroupeClasse("GroupeClasse");
 
